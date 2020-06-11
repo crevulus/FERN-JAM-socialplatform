ff40c53c90fd59d936d98056a51395a88fe427ad
crevulus <crevulus@gmail.com>
2020-06-11 12:52:10 +0200

    getting data back from firebase mentions collection

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+admin.initializeApp();
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+exports.getMentions = functions.https.onRequest((req, res) => {
+  admin.firestore().collection('mentions').get()
+    .then(data => {
+      let mentions = [];
+      // data = reference to data docs, not data itself
+      data.forEach((doc) => {
+        mentions.push(doc.data());
+      });
+      return res.json(mentions);
+    })
+    .catch(err => new Error(err));
+});
