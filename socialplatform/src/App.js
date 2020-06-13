f9432e57a26cd144e358a29750bbdf0bb850cf8c
crevulus <crevulus@gmail.com>
2020-06-13 13:32:32 +0200

    Navbar & routing buttons

--- socialplatform/src/App.js
+++ socialplatform/src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch,
+} from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import NavBar from "./components/NavBar";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Social Platform app</h1>
+      <Router>
+        <NavBar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ socialplatform/src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Link from "react-router-dom/Link";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Button color="inherit" component={Link} to="/">
+              Home
+            </Button>
+            <Button color="inherit" component={Link} to="/login">
+              Login
+            </Button>
+            <Button color="inherit" component={Link} to="/signup">
+              Signup
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
