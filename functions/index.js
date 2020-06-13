4eba4412684b9fb0c67e348096a29c2ae20743e6
crevulus <crevulus@gmail.com>
2020-06-13 11:19:52 +0200

    db trigger for image change working

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const firebase = require("firebase");
+const { db } = require("./util/admin");
 
 const {
   getAllMentions,
@@ -43,3 +44,54 @@ app.get("/user", firebaseAuth, getAuthUser);
 // exports function that handles HTTP events
 // specifies zone and saves few hundred ms of latency
 exports.api = functions.region("europe-west3").https.onRequest(app);
+
+exports.onUserImageChange = functions
+  .region("europe-west3")
+  .firestore.document(`/users/{userId}`)
+  // snapshot has two values: before and after.
+  .onUpdate((change) => {
+    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+      // writes batches; commited with .commit()
+      const batch = db.batch();
+      return db
+        .collection("mentions")
+        .where("userHandle", "==", change.before.data().userHandle)
+        .get()
+        .then((data) => {
+          // forEach document this user has created
+          data.forEach((doc) => {
+            const mention = db.doc(`/mentions/${doc.id}`);
+            batch.update(mention, { userImage: change.after.data().imageUrl });
+          });
+          return batch.commit();
+        });
+    } else return true;
+  });
+
+exports.onMentionDeleted = functions
+  .region("europe-west3")
+  .firestore.document(`/users/{mentionId}`)
+  .onDelete((snapshot, context) => {
+    const mentionId = context.params.mentionId;
+    const batch = db.batch();
+    return db
+      .collection("comments")
+      .where("mentionId", "==", mentionId)
+      .get()
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/comments/${doc.id}`));
+        });
+        return db.collection("likes").where("mentionId", "==", mentionId).get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/likes/${doc.id}`));
+        });
+        return batch.commit();
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+      });
+  });
