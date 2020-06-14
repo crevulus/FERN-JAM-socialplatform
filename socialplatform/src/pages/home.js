5db541221cc09f323413c0724f1e2bb6196f1cf8
crevulus <crevulus@gmail.com>
2020-06-14 21:29:37 +0200

    using material UI grid

--- socialplatform/src/pages/home.js
+++ socialplatform/src/pages/home.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Home</h1>
-      </div>
+      <Grid container>
+        <Grid item sm={8} xs={12}>
+          <p>Content Left</p>
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile Right</p>
+        </Grid>
+      </Grid>
     );
   }
 }
