0309e7c5bb6690f833be725148864d0a0c350014
crevulus <crevulus@gmail.com>
2020-06-15 12:28:25 +0200

    started login form

--- socialplatform/src/pages/login.js
+++ socialplatform/src/pages/login.js
@@ -4,21 +4,84 @@ import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Grid from "@material-ui/core/Grid";
 import icon from "../magoosh-ticks.png";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const styles = {
   form: {
     textAlign: "center",
   },
+  logo: {
+    margin: 10,
+  },
+  button: {
+    marginTop: 15,
+  },
 };
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      errors: [],
+    };
+  }
+
+  handleSubmit = (event) => {
+    console.log("submitted");
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
     const { classes } = this.props;
     return (
       <Grid container className={classes.form}>
         <Grid item sm />
         <Grid item sm>
-          <img src={icon} alt="Logo" />
+          <img src={icon} alt="Logo" className={classes.logo} />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.textField}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className={classes.textField}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+              color="primary"
+              className={classes.button}
+            >
+              Log in
+            </Button>
+          </form>
         </Grid>
         <Grid item sm />
       </Grid>
