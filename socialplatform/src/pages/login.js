3afa92a034257c546f4f7bc2fdae245ef4fff1c6
crevulus <crevulus@gmail.com>
2020-06-15 12:58:41 +0200

    trying to add spinner

--- socialplatform/src/pages/login.js
+++ socialplatform/src/pages/login.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
+import Link from "react-router-dom/Link";
 
 import withStyles from "@material-ui/core/styles/withStyles";
 import Grid from "@material-ui/core/Grid";
@@ -8,6 +9,7 @@ import icon from "../magoosh-ticks.png";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const styles = {
   form: {
@@ -22,6 +24,7 @@ const styles = {
   customError: {
     color: "red",
     fontSize: "0.8rem",
+    marginTop: 8,
   },
 };
 
@@ -118,7 +121,12 @@ class Login extends Component {
               className={classes.button}
             >
               Log in
+              {loading && <CircularProgress color="secondary" size="30" />}
             </Button>
+            <p>
+              If you haven't created an account yet, you can make one{" "}
+              <Link to="/signup">here</Link>.
+            </p>
           </form>
         </Grid>
         <Grid item sm />
