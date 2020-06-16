e7dd906bef7496e6e6752f585aaffeedc5b301e0
crevulus <crevulus@gmail.com>
2020-06-16 21:29:44 +0200

    fixed userToken issue; invalid paylod value still a problem

--- socialplatform/src/pages/signup.js
+++ socialplatform/src/pages/signup.js
@@ -11,6 +11,9 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
+import { connect } from "react-redux";
+import { signupUser } from "../redux/actions/userActions";
+
 const styles = {
   form: {
     textAlign: "center",
@@ -36,9 +39,14 @@ class Signup extends Component {
       password: "",
       confirmPassword: "",
       userHandle: "",
-      loading: false,
       errors: [],
     };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.UI.errors) {
+      this.setState({ errors: nextProps.UI.errors });
+    }
   }
 
   handleSubmit = (event) => {
@@ -52,23 +60,7 @@ class Signup extends Component {
       confirmPassword: this.state.confirmPassword,
       userHandle: this.state.userHandle,
     };
-    axios
-      .post("/signup", newUserData)
-      .then((res) => {
-        localStorage.setItem("firebaseIdToken", `Bearer ${res.data.userToken}`);
-        this.setState({
-          loading: false,
-        });
-        // redirect to home
-        this.props.history.push("/");
-      })
-      .catch((err) => {
-        console.log(err.response);
-        this.setState({
-          errors: err.response.data,
-          loading: false,
-        });
-      });
+    this.props.signupUser(newUserData, this.props.history); // history can redirect us on success
   };
 
   handleChange = (event) => {
@@ -78,8 +70,11 @@ class Signup extends Component {
   };
 
   render() {
-    const { classes } = this.props;
-    const { errors, loading } = this.state;
+    const {
+      classes,
+      UI: { loading },
+    } = this.props;
+    const { errors } = this.state;
     return (
       <Grid container className={classes.form}>
         <Grid item sm />
@@ -167,6 +162,16 @@ class Signup extends Component {
 
 Signup.propTypes = {
   classes: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired,
+  signupUser: PropTypes.func.isRequired,
 };
 
-export default withStyles(styles)(Signup);
+const mapStateToProps = (state) => ({
+  user: state.user,
+  UI: state.UI,
+});
+
+export default connect(mapStateToProps, { signupUser })(
+  withStyles(styles)(Signup)
+);
--- socialplatform/src/redux/actions/userActions.js
+++ socialplatform/src/redux/actions/userActions.js
@@ -1,4 +1,10 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from "../types";
+import {
+  SET_USER,
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  LOADING_UI,
+  SET_UNAUTHENTICATED,
+} from "../types";
 import axios from "axios";
 
 export const loginUser = (userData, history) => (dispatch) => {
@@ -12,7 +18,9 @@ export const loginUser = (userData, history) => (dispatch) => {
         loading: false,
       });
       // every time we send a req with axios it will have a an auth header with our token
-      axios.defaults.common["Authorization"] = `Bearer ${res.data.token}`;
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${res.data.token}`;
       dispatch(getUserData());
       dispatch({ type: CLEAR_ERRORS });
       // redirect to home
@@ -25,6 +33,41 @@ export const loginUser = (userData, history) => (dispatch) => {
         payload: err.response.data, // this is where we have the undefined problem...
       });
     });
+};
+
+export const signupUser = (newUserData, history) => (dispatch) => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post("/signup", newUserData)
+    .then((res) => {
+      console.log(res.data);
+      localStorage.setItem("firebaseIdToken", `Bearer ${res.data.userToken}`);
+      this.setState({
+        loading: false,
+      });
+      // every time we send a req with axios it will have a an auth header with our token
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${res.data.userToken}`;
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      // redirect to home
+      history.push("/");
+    })
+    .catch((err) => {
+      console.log("got into catch before dispatch");
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data, // this is where we have the undefined problem...
+      });
+    });
+};
+
+export const logoutUser = () => (dispatch) => {
+  localStorage.removeItem("firebaseIdToken");
+  // remove auth header from axios defaults
+  delete axios.defaults.headers.common["Authorization"];
+  dispatch({ type: SET_UNAUTHENTICATED }); // clear our user state
 };
 
 export const getUserData = () => (dispatch) => {
