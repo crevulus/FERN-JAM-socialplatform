b38eb5753fc0c375a85f18e6aed365fff39ccb75
crevulus <crevulus@gmail.com>
2020-06-17 13:56:00 +0200

    profile info on right-side of home

--- socialplatform/src/components/Profile.js
+++ socialplatform/src/components/Profile.js
@@ -1,14 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
+import Link from "react-router-dom/Link";
+
+import { connect } from "react-redux";
 
 import withStyles from "@material-ui/core/styles/withStyles";
 import Button from "@material-ui/core/Button";
-import { connect } from "react-redux";
 import Paper from "@material-ui/core/Paper";
+import MaterialLink from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
+
+import LocationOn from "@material-ui/icons/LocationOn";
+import CalendarToday from "@material-ui/icons/CalendarToday";
+import LinkIcon from "@material-ui/icons/Link";
 
 const styles = {
   paper: {
+    padding: 20,
+  },
+  profilePic: {
     maxWidth: 50,
+  },
+  profileDetails: {
+    color: "red",
   },
 };
 
@@ -33,7 +47,25 @@ class Profile extends Component {
     let profile = !loading ? (
       authenticated ? (
         <Paper className={classes.paper}>
-          <img src={imageUrl} alt="Profile" />
+          <div className={classes.container}>
+            <img className={classes.profilePic} src={imageUrl} alt="Profile" />
+          </div>
+          <div className={classes.profileDetails}>
+            <MaterialLink
+              component={Link}
+              to={`/users/${userHandle}`}
+              color="primary"
+              variant="h6"
+            >
+              {userHandle}
+            </MaterialLink>
+            {bio && <Typography variant="body2">{bio}</Typography>}
+            {location && (
+              <Fragment>
+                <LocationOn /> <div>{location}</div>
+              </Fragment>
+            )}
+          </div>
         </Paper>
       ) : (
         <p>Not authorized</p>
