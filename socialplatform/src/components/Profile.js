629e6ec7cf0cd4a8b4491133e7f3414bf145825f
crevulus <crevulus@gmail.com>
2020-06-17 14:22:47 +0200

    finished profile widget, need to format

--- socialplatform/src/components/Profile.js
+++ socialplatform/src/components/Profile.js
@@ -2,6 +2,8 @@ import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import Link from "react-router-dom/Link";
 
+import dayjs from "dayjs";
+
 import { connect } from "react-redux";
 
 import withStyles from "@material-ui/core/styles/withStyles";
@@ -20,9 +22,13 @@ const styles = {
   },
   profilePic: {
     maxWidth: 50,
+    borderRadius: "50%",
   },
   profileDetails: {
     color: "red",
+  },
+  buttons: {
+    padding: 5,
   },
 };
 
@@ -47,24 +53,23 @@ class Profile extends Component {
     let profile = !loading ? (
       authenticated ? (
         <Paper className={classes.paper}>
-          <div className={classes.container}>
-            <img className={classes.profilePic} src={imageUrl} alt="Profile" />
-          </div>
+          <img className={classes.profilePic} src={imageUrl} alt="Profile" />
+          <MaterialLink
+            component={Link}
+            to={`/users/${userHandle}`}
+            color="primary"
+            variant="h6"
+          >
+            {userHandle}
+          </MaterialLink>
           <div className={classes.profileDetails}>
-            <MaterialLink
-              component={Link}
-              to={`/users/${userHandle}`}
-              color="primary"
-              variant="h6"
-            >
-              {userHandle}
-            </MaterialLink>
             {bio && <Typography variant="body2">{bio}</Typography>}
             {location && (
               <Fragment>
-                <LocationOn /> <div>{location}</div>
+                <LocationOn /> <span>{location}</span>
               </Fragment>
             )}
+            <br />
             {website && (
               <Fragment>
                 <LinkIcon />
@@ -75,10 +80,37 @@ class Profile extends Component {
                 </a>
               </Fragment>
             )}
+            <br />
+            <Fragment>
+              <CalendarToday />
+              <span>Joined {dayjs(createdAt).format("MMM YYYY")}</span>
+            </Fragment>
           </div>
         </Paper>
       ) : (
-        <p>Not authorized</p>
+        <Paper className={classes.paper}>
+          <Typography variant="body2" align="center">
+            No profile found, please login again
+          </Typography>
+          <div className={classes.buttons}>
+            <Button
+              variant="contained"
+              color="primary"
+              component={Link}
+              to="/login"
+            >
+              Login
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              component={Link}
+              to="/signup"
+            >
+              Signup
+            </Button>
+          </div>
+        </Paper>
       )
     ) : (
       <p>Loading...</p>
